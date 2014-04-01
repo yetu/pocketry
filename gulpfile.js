8ca76a79ed553272133c969de839fc3f230f37e8
FND <fnd@localhost.localdomain>
2014-04-01 07:12:59 +0200

    extended build script to include jqLite shim for AngularJS

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,20 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('dist', function() {
-	return gulp.src(['lib/ui.js', 'lib/layout.js', 'lib/draggable.js']).
-		pipe(concat('pocketry.js')).
+	var paths = ['lib/ui.js', 'lib/layout.js', 'lib/draggable.js'];
+	build(paths);
+	build(['lib/angular_shim.js'].concat(paths), 'angular');
+});
+
+function build(paths, suffix) {
+	var name = 'pocketry';
+	if(suffix) {
+		name += '-' + suffix;
+	}
+	gulp.src(paths).
+		pipe(concat(name + '.js')).
 		pipe(gulp.dest('dist')).
-		pipe(rename('pocketry.min.js')).
+		pipe(rename(name + '.min.js')).
 		pipe(uglify()).
 		pipe(gulp.dest('dist'));
-});
+}
