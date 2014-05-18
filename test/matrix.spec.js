e16a549a5aaba29cb003bc7457ae7367e0ec9661
Mr_Mig <alexey.migutsky@gmail.com>
2014-05-18 17:41:43 +0300

    Update matrix methods definitions.
    Change definitions to be more natural regarding the monadic nature of operations.

--- test/matrix.spec.js
+++ test/matrix.spec.js
@@ -1,6 +1,7 @@
 /*jslint sloppy:true, nomen:true */
-/*global describe, it, expect, Matrix, beforeEach */
-describe('Matrix suite', function () {
+/*global jasmine, describe, xdescribe, it, xit, expect, beforeEach,
+ Matrix */
+describe('Matrix', function () {
   var arr, m;
 
   function id(x) {
@@ -10,7 +11,8 @@ describe('Matrix suite', function () {
   beforeEach(function () {
     arr = [
       [1, 2, 3, 4, 5],
-      [9, 8, 7, 6, 5]
+      [9, 8, 7, 6, 5],
+      [10, 11, 12, 13, 14]
     ];
 
     m = new Matrix(arr);
@@ -24,110 +26,158 @@ describe('Matrix suite', function () {
     expect(new Matrix(arr)._).toEqual(arr);
   });
 
-  it('should get an element by index', function () {
-    expect(new Matrix(arr).get(1, 0)).toEqual(9);
-  });
-
-  it('should has selection methods to choose rows and columns', function () {
-    expect(m.rows(1).isRowSelected(1)).toBeTruthy();
-    expect(m.rows(1).isRowSelected(0)).toBeFalsy();
-    expect(m.rows(0).isRowSelected(0)).toBeTruthy();
-    expect(m.rows(0).isRowSelected(1)).toBeFalsy();
-  });
-
-  it('should has map method returning the mapped matrix values', function () {
-    var sub = [
-      2, 3, 4
-    ];
-
-    m.rows(0).cols(1, 3).map(id)[0].forEach(function (val, idx) {
-      expect(val).toBe(sub[idx]);
+  describe('.get', function () {
+    it('should get an element by index', function () {
+      expect(new Matrix(arr).get(1, 0)).toEqual(9);
     });
 
+    it('should get an undefined value on virtual element', function () {
+      expect(m.get(100, 100)).toBeUndefined();
+    });
+
+    it('should return value when specified 2 args', function () {
+      expect(m.get(1, 1)).toEqual(arr[1][1]);
+    });
+
+    it('should return a row when specified 1 arg', function () {
+      expect(m.get(1)).toEqual(arr[1]);
+    });
+
+    it('should return an array matrix when no args specified ',
+      function () {
+        expect(m.get()).toEqual(arr);
+      });
+
+    it('should return correct array when cells are empty', function () {
+      arr = [
+        [null, null],
+        [null, null]
+      ];
+
+      m = new Matrix(arr);
+
+      expect(m.get()).toEqual(arr);
+    });
   });
-
-  it('method `some` should return true ' +
-    'if predicate match on any matrix value',
-    function () {
-      expect(m.rows(1).cols(0, 3).some(function (val) {
-        return val === 7;
-      })).toBeTruthy();
-    });
-
-  it('method `some` should return false ' +
-    'if predicate does not match on some matrix value',
-    function () {
-      expect(m.rows(1).cols(0, 3).some(function (val) {
-        return val === 1;
-      })).toBeFalsy();
-    });
-
-  it('method `each` should iterate over each selected matrix value', function () {
-    m.rows(0).cols(1, 3).each(function (val, rowIdx, colIdx) {
-      expect(m.get(rowIdx, colIdx)).toEqual(val);
-    });
-  });
-
-  it('`each` iterator should overwrite wrapped values', function () {
-    m.rows(0, 0).cols(0, 0).each(function (val, rowId, colId, put) {
-      put(101);
-    });
-
-    expect(m.get(0, 0)).toEqual(101);
-  });
-
-  it('`each` should write into empty virtual cells', function () {
-    m = new Matrix();
-
-    var arr = [
-      [1, 1],
-      [2, 2]
-    ];
-
-    m.rows(0, 1).cols(0, 1).each(function (val, rid, cid, put) {
-      put(rid + 1);
-    });
-
-    expect(m.get()).toEqual(arr);
-  });
-
 
   it('should have method to set values', function () {
     m.put(1, 1, 101);
     expect(m.get(1, 1)).toEqual(101);
   });
 
+  it('.map should iterate and write each real matrix cell', function () {
+    var result = [
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1]
+    ];
+
+    expect(m.map(function (val) {
+      return 1;
+    }).get()).toEqual(result);
+  });
+
   it('should have method to get matrix size', function () {
     expect(m.size()).toEqual([arr.length, arr[0].length]);
   });
 
-  it('should have method to add rows', function () {
-    var oldSize = m.size(),
-      newSize = [
-        oldSize[0] + 1,
-        oldSize[1]
-      ];
-    expect(m.appendRow().size()).toEqual(newSize);
+  it('.each should iterate over all matrix values', function () {
+    var iterations = 0;
+    m.each(function (val, rowId, colId) {
+      expect(val).toEqual(arr[rowId][colId]);
+      iterations++;
+    });
+    expect(iterations).toEqual(m.size()[0] * m.size()[1]);
   });
 
-  it('should append specified row to the matrix', function () {
-    var row = [12, 13, 14, 15, 16];
-    m.appendRow(row);
-    expect(m.rows(2, 2).map(id)[0]).toEqual(row);
+  describe('.some', function () {
+    it('method `some` should return true ' +
+      'if predicate match on any matrix value',
+      function () {
+        expect(m.some(function (val) {
+          return val === 7;
+        })).toBeTruthy();
+      });
+
+    it('method `some` should return false ' +
+      'if predicate does not match on some matrix value',
+      function () {
+        expect(m.some(function (val) {
+          return val === 11111;
+        })).toBeFalsy();
+      });
   });
 
-  it('method `get` should return value when specified 2 args', function () {
-    expect(m.get(1, 1)).toEqual(arr[1][1]);
-  });
-
-  it('method `get` should return a row when specified 1 arg', function () {
-    expect(m.get(1)).toEqual(arr[1]);
-  });
-
-  it('method `get` should return an array matrix' +
-    ' when no args specified ',
-    function () {
-      expect(m.get()).toBe(arr);
+  describe('MatrixSelection', function () {
+    it('.val should get the current selection values', function () {
+      expect(m.rows(100).cols(100).val()).toBeUndefined();
     });
 
+    describe('.each', function () {
+      it('should iterate over all selected values (real and virtual)', function () {
+        var iterations = 0;
+        m.rows(0, 9).cols(1, 3).each(function (val, row, col) {
+          expect(m.get(row, col)).toEqual(val);
+          iterations++;
+        });
+        expect(iterations).toEqual(30);
+      });
+    });
+
+    describe('.map', function () {
+      it('should return a matrix instance', function () {
+        expect(m.rows(0).cols(0).map(function () {
+          return 1;
+        })).toEqual(jasmine.any(Matrix));
+        return;
+      });
+
+      it('should write each function result value to each cell', function () {
+        arr[0] = [1, 1, 1, 1, 1];
+        expect(m.rows(0).map(function () {
+          return 1;
+        }).get()).toEqual(arr);
+      });
+
+      it('.map.matrix.get() should return correct array', function () {
+        arr = [
+          [1, 1],
+          [1, 1]
+        ];
+
+        m = new Matrix();
+        m.rows(0, 1).cols(0, 1).map(function () {
+          return 1;
+        });
+        expect(m.get()).toEqual(arr);
+      });
+
+      it('should create empty cell when no function is specified', function () {
+        var m = new Matrix(),
+          result = [
+            [null, null],
+            [null, null]
+          ];
+
+        m.rows(0, 1).cols(0, 1).map();
+        expect(m.get()).toEqual(result);
+      });
+    });
+
+    describe('.some', function () {
+      it('should return true if predicate match on any matrix value',
+        function () {
+          expect(m.rows(1).cols(0, 3).some(function (val) {
+            return val === 7;
+          })).toBeTruthy();
+        });
+
+      it('should return false if predicate does not match on some matrix value',
+        function () {
+          expect(m.rows(1).cols(0, 3).some(function (val) {
+            return val === 1;
+          })).toBeFalsy();
+        });
+    });
+  });
 });
