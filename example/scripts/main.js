033f10e04647a4cae1c732d07f0e6757c7cda356
Guilherme Oliveira <ghophp@gmail.com>
2015-05-20 09:41:11 +0200

    remove triple instance test

--- example/scripts/main.js
+++ example/scripts/main.js
@@ -5,8 +5,6 @@
 'use strict';
 
 var board = new Pocketry('.tiles', 2, { interactive: true });
-board = new Pocketry('.tiles', 2, { interactive: true });
-board = new Pocketry('.tiles', 2, { interactive: true });
 
 // debug rendering
 console.debug(LOG.serializeStack(board.layout.stack));
